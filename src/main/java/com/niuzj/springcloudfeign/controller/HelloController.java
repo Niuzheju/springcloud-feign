84176545fbece9a65595347ab2941c9a47ce329b
niuzj <2549218666@qq.com>
2019-02-12 16:56:21 +0800

    ok

--- src/main/java/com/niuzj/springcloudfeign/controller/HelloController.java
+++ src/main/java/com/niuzj/springcloudfeign/controller/HelloController.java
@@ -1,5 +1,6 @@
 package com.niuzj.springcloudfeign.controller;
 
+import com.niuzj.springcloudfeign.model.User;
 import com.niuzj.springcloudfeign.service.IHelloService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -17,11 +18,26 @@ public class HelloController {
     private String result = "success";
 
     @RequestMapping("/hello")
-    public String hello(){
+    public String hello() {
         Map<String, Object> map = new HashMap<>();
         String hello = helloService.hello("nzj", "zyq", map);
         System.out.println(hello);
         return result;
     }
 
+    @RequestMapping("/uri")
+    public String uri() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("name", "nzj");
+        User user = helloService.uri(map);
+        System.out.println(user);
+        return result;
+    }
+
+    @RequestMapping("/addUser")
+    public String addUser() {
+        String user = helloService.addUser(new User("牛哲举", 24));
+        System.out.println(user);
+        return result;
+    }
 }
--- src/main/java/com/niuzj/springcloudfeign/service/IHelloService.java
+++ src/main/java/com/niuzj/springcloudfeign/service/IHelloService.java
@@ -1,10 +1,15 @@
 package com.niuzj.springcloudfeign.service;
 
+import com.niuzj.springcloudfeign.model.User;
 import org.springframework.cloud.netflix.feign.FeignClient;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.Map;
 
+/**
+ * spring cloud feign 把要调用的http服务接口和java接口绑定在一起,方便调用
+ * 接口绑定使用springMVC注解
+ */
 @FeignClient("provider")
 public interface IHelloService {
 
@@ -12,13 +17,16 @@ public interface IHelloService {
     String hello(@RequestParam(name = "name") String name, @RequestHeader("airen") String airen, @RequestBody Map<String, Object> map);
 
     @RequestMapping("/uri")
-    String uri(@RequestBody Map<String, Object> map);
+    User uri(@RequestBody Map<String, Object> map);
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.PUT)
     String put(@PathVariable("id") Integer id);
 
     @RequestMapping(value = "/user/{id}", method = RequestMethod.DELETE)
     String delete(@PathVariable("id") Integer id);
+
+    @RequestMapping("/addUser")
+    String addUser(@RequestBody User user);
 
 
 
